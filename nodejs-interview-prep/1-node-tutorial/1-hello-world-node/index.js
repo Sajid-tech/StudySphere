ca9e05b206c06316d560edd5502a55aa84b7c8a0
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-06-30 23:12:36 +0530

    `1-hello world-node done`

---
+++ nodejs-interview-prep/1-node-tutorial/1-hello-world-node/index.js
@@ -0,0 +1,10 @@
+console.log("Hello nodejs")
+
+
+const array = [1,2,3,4]
+console.log("array:",array)
+
+setTimeout(()=>{
+
+    console.log('this is last line of sync code ')
+},2000)
---
+++ nodejs-interview-prep/1-node-tutorial/1-hello-world-node/sum.js
@@ -0,0 +1,10 @@
+// const sum = (a,b)=>{
+//     return a+ b
+// }
+
+
+function sum(a,b){
+    return a+b
+}
+
+console.log(sum(5,6))
