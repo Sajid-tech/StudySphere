5441cc5c4b61ae91a86f09f592eaedc17a4b646e
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-07-29 17:20:47 +0530

    express js middleware defination

--- nodejs-interview-prep/2-express/middleware.js
+++ nodejs-interview-prep/2-express/middleware.js
@@ -1,3 +1,9 @@
+// In Node.js with Express, a middleware is a function that runs between the request and the response.
+
+// Think of it like a checkpoint or filter that runs before the final response is sent to the client.
+
+// Middleware is a function that has access to the request (req), response (res), and a next() function that tells Express to move to the next middleware or route. 
+
 const express = require('express')
 
 const app = express()
