a4133a8897fc916365886977aaf57d8c39b40597
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-08-04 22:48:04 +0530

    `mongodb final `

--- nodejs-interview-prep/5-mongodb-basic/app.js
+++ nodejs-interview-prep/5-mongodb-basic/app.js
@@ -25,27 +25,27 @@ const User = mongoose.model("User", UserSchema);
 async function runQueryExamples() {
   try {
     // create a new documnets -- by two way
-    /*
+ 
     const newUser = await User.create({
-      name: "Sajid",
+      name: "updated User",
       email: "sajid@gmail.com",
-      age: 12,
+      age: 2,
       isActive: true,
       tags: ["designer", "developer", "manager"],
     });
- */
+
     // or
 
     // const newUser =  new User({
-    //        name: "Raj Mukherjee",
-    //       email: "raj@gmail.com",
-    //       age: "40",
+    //        name: "Travis Mukherjee",
+    //       email: "travis@gmail.com",
+    //       age: "55",
     //       isActive: true,
     //       tags: ["developer", "designer", "manager"],
     // })
 
     // await newUser.save()
-    // console.log("created new user", newUser);
+    console.log("created new user", newUser);
 
     // get all user
 
@@ -63,8 +63,45 @@ async function runQueryExamples() {
     // const getSajidUser = await User.findOne({name:"Sajid"})
     // console.log(getSajidUser)
 
-    const getLastCreatedUserByUserId = await User.findById(newUser._id);
-    console.log(getLastCreatedUserByUserId, "getLastCreatedUserByUserId");
+    // find the id  
+
+    // const getLastCreatedUserByUserId = await User.findById(newUser._id)
+    // or we can pass _id in place of newuser._id
+    // console.log("getLastCreateduserbyUserId",getLastCreatedUserByUserId)
+
+    // selected property
+    // -_id minus here it means exclude that property
+    // const selectedFields = await User.find().select("name email -_id")
+    // console.log("selectedField",selectedFields)
+
+    // this is for eg. pagaination to get limited data 
+    // it means give me 2 data but skip the first data 
+    // const limitedUsers = await User.find().limit(2).skip(1);
+    // console.log("limited User",limitedUsers)
+
+    // sorting 
+    // sort the data with age but in descending order -1 : descending , +1 : ascending
+
+    // const sortedUser = await User.find().sort({age:-1})
+    // console.log(sortedUser)
+
+
+    // count the documents where isActive is true : give teh number of document: length
+    // const countDocuments = await User.countDocuments({isActive:true})
+    // console.log("countDocument",countDocuments)
+
+    // delete the user 
+
+    // const deletedUser = await User.findByIdAndDelete(newUser._id)
+    // console.log("deelted user",deletedUser)
+
+
+    // updated user 
+
+    const updatedUser = await User.findByIdAndUpdate(newUser._id , {
+      $set : {age:100}, $push:{tags:'updated'}
+    },{new:true})
+    console.log("Updated User",updatedUser)
   } catch (error) {
     console.log("Error:", error);
   } finally {
